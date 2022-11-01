66eb9be9b474beddf94bcb6c2b35aef35a3ee8e0
Patrick Apgar <44283314+patricklapgar@users.noreply.github.com>
2022-11-01 18:42:26 -0500

    Changed Reverse Linked List

--- LeetCamp6/ReverseLinkedList_Recursive.py
+++ LeetCamp6/ReverseLinkedList_Recursive.py
@@ -9,30 +9,14 @@ class ListNode:
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def recurse(
-            curr: Optional[ListNode], prev: Optional[ListNode]
-        ) -> Optional[ListNode]:
-            """Recurses through a singly-linked list until it `curr` is `None`.
+        
+        if (not head) or (not head.next):
+            return head
 
-            Arguments:
-            curr: ListNode - current list node
-            prev: ListNode - previous list node
-
-            Returns:
-            ListNode - Returns the original tail node, which will be the new head node
-            """
-            if not curr:  # Base Case
-                return prev  # return previous, because curr is None
-
-            # Save the next node, because we will replace curr.next
-            next_node = curr.next
-            curr.next = prev  # curr.next will now point **backwards** to prev
-
-            # recurse to the next_node, update prev with curr
-            return recurse(next_node, curr)
-
-        # Will return the tail (new head) once the base case occurs
-        return recurse(head, None)
+        next = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return next
 
     def print_list(self, head: Optional[ListNode]):
         # Print val of each node (with a next arrow)
